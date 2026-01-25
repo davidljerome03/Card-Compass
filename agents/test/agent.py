2a34b91d595bdc92abf5128827e9ca012aa2e99f
Kyle-Carter <kyle.carter1105@gmail.com>
2026-01-25 01:03:17 +0000

    Struggling to commit, hopefully this pushes everything with new agents and backend structure

--- agents/test/agent.py
+++
@@ -1,8 +0,0 @@
-from google.adk.agents.llm_agent import Agent
-
-root_agent = Agent(
-    model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant to find what is located at gps coordinates.',
-    instruction='User MCP tools to find the building at a location',
-)
